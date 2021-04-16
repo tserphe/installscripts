577ea356a9f013fbef9e45e905e38d03f2c98b69
tserphe <82549954+tserphe@users.noreply.github.com>
2021-04-16 18:25:50 -0500

    Update arch_install.sh

--- arch_install.sh
+++ arch_install.sh
@@ -8,7 +8,6 @@ SWAPSIZE="1024"
 DEVICE="/dev/sda"
 PARTITION="/dev/sda1"
 LOCALE="en_US.UTF-8 UTF-8"
-REBOOT="true"
 
 # Get hostname variable
 read -p 'Hostname: ' HOSTNAME
@@ -21,22 +20,18 @@ ping -c 1 -W 5 -w 10 archlinux.org
 
 # Update the system clock
 timedatectl set-ntp true
-sleep 2
 
 # Format Partition
 mkfs.ext4 -L ROOT $PARTITION
-sleep 2
 
 # Mount Partition
 mount $PARTITION /mnt
-sleep 2
 
 # Update mirrorlist
 reflector --country "United States" --latest 25 --age 24 --protocol https --completion-percent 100 --sort rate --save /etc/pacman.d/mirrorlist
 
 # Install essential packages
 pacstrap /mnt base base-devel linux-lts linux-firmware intel-ucode networkmanager htop neovim
-sleep 5
 
 # Generate fstab
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -53,17 +48,14 @@ echo "vm.swappiness=1" > /mnt/etc/sysctl.d/99-swappiness.conf
 
 # Enable fstrim
 arch-chroot /mnt systemctl enable fstrim.timer
-sleep 5
 
 # Set time zone
 arch-chroot /mnt ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
 arch-chroot /mnt hwclock --systohc
-sleep 3
 
 # Uncomment locale and gen locale
 sed -i "s/#$LOCALE/$LOCALE/" /mnt/etc/locale.gen
 arch-chroot /mnt locale-gen
-sleep 3
 echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
 
 # Create hostname file
@@ -74,7 +66,6 @@ echo -e "127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\t$HOSTNAME.localdomai
 
 # Enable Network Manager
 arch-chroot /mnt systemctl enable NetworkManager.service
-sleep 5
 
 # Set root password
 printf "$ROOT_PASSWORD\n$ROOT_PASSWORD" | arch-chroot /mnt passwd
@@ -91,26 +82,4 @@ curl https://raw.githubusercontent.com/tserphe/LARBS/master/larbs.sh >> /mnt/roo
 umount -R /mnt
 
 # Install completed. Reboot
-echo "Arch Linux installed successfully"'!'
-    echo ""
-
-    if [ "$REBOOT" == "true" ]; then
-        REBOOT="true"
-
-        set +e
-        for (( i = 15; i >= 1; i-- )); do
-            read -r -s -n 1 -t 1 -p "Rebooting in $i seconds... Press any key to abort."$'\n' KEY
-            if [ $? -eq 0 ]; then
-                REBOOT="false"
-                break
-            fi
-        done
-        set -e
-
-        if [ "$REBOOT" == 'true' ]; then
-            echo "Rebooting..."
-            echo ""
-            sleep 3
-            reboot
-        fi
-    fi
+echo "Arch Linux installed successfully"'!'" Please type 'reboot'."
